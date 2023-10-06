543c369079a6ff2267637638f079f2ae11bdfe3a
Vitor <vitorcooutinho@gmail.com>
2023-10-06 20:30:10 -0300

    Ia - project

--- Projeto1/Rules_induction.py
+++ Projeto1/Rules_induction.py
@@ -1,31 +1,61 @@
 '''
-Universidade Estadual de Santa Cruz - UESC
-Autores: Kauan Souza, Luíz Rosário e Vítor Coutinho
-Disciplina: Inteligência Artificial
-
+    Universidade Estadual de Santa Cruz - UESC
+    Autores: Kauan Souza, Luíz Rosário e Vítor Coutinho
+    Disciplina: Inteligência Artificial
 '''
 
 def map_reader(file_name):
     '''
-    Função responsável por ler os dados do arquivo csv e 
-    armazená-los em um dicionário.
+        Função responsável por ler os dados do arquivo csv e 
+        armazená-los em um dicionário.
 
-    Parâmetros: path do arquivo csv
-    Retorno: dicionário com os dados do arquivo csv
-
+        Parâmetros: path do arquivo csv
+        Retorno: Lista com os dados do arquivo csv
     '''
 
-    data = {} # Dicionário que armazena os dados do arquivo csv
-    values = () # Tupla que armazena os valores de cada linha do arquivo csv
-
+    header = []
+    dados = []
     with open(file_name, "r", encoding="utf-8") as archive:
+        header = archive.readline().strip().split(",")
         for line in archive:
-            values = line.strip().split(",") # Separa os valores de cada linha do arquivo csv
-            index = values[0] # Armazena o índice da linha
+            line = line.strip().split(",")
+            map_data = {}
 
-            # Armazena os valores da linha no dicionário
-            data[index] = values[1:]
-    return data
+            for i, header_value in enumerate(header):
+                map_data[header_value] = line[i]
 
-for key, value in map_reader("Projeto1/Teste.csv").items():
-    print(f"{key}: {value}")
+            dados.append(map_data)
+
+    return dados, header
+
+def get_values(dados, header):
+    '''
+        Função responsável por contar a quantidade de valores
+        de cada atributo.
+
+        Parâmetros: lista com os dados do arquivo csv e o cabeçalho
+        Retorno: Dicionário com os possivéis valores de cada atributo
+    '''
+
+    values = {}
+    for header_value in header:
+        values[header_value] = []
+
+    for dado in dados:
+        for header_value in header:
+            if dado[header_value] not in values[header_value]:
+                values[header_value].append(dado[header_value])
+
+    return values
+
+
+data_list, headers = map_reader("Projeto1/Teste.csv")
+possible_values = get_values(data_list, headers)
+
+for value in data_list:
+    print(value)
+
+print()
+
+for key, value in possible_values.items():
+    print(key, value)
